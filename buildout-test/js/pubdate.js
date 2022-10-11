7f1785bf5197fbb32e2012e9b657487d5ec12e53
Jacob Day <26262942+jacobday@users.noreply.github.com>
2022-10-10 21:28:00 -0400

    fixed metadata.json

--- buildout-test/js/pubdate.js
+++
@@ -1,7 +0,0 @@
-// TODO: replace title and slug based on metadata
-fetch("../metadata.json")
-    .then(response => response.json())
-    .then(data => {
-        document.querySelector('#pubdate').innerHTML = "Published " + data.pub_date;
-    })
-    .catch(err => console.log(err))
